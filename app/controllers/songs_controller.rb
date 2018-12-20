6e95beaa831448b14d1988f63d8d555306386239
Ally Netzel <netzelally@gmail.com>
2018-12-20 03:12:07 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,12 @@
 class SongsController < ApplicationController
     require 'csv'
 
+  def upload
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    end
+    redirect_to songs_path
+  end
 
   def index
     @songs = Song.all
